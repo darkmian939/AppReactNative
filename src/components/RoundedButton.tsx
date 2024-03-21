5cdb7f825123269776db125620a5963491dc6b18
sena <140548399+darkmian939@users.noreply.github.com>
2024-03-21 17:46:37 -0500

    guardar lo siguiente del celular

---
+++ src/components/RoundedButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TouchableOpacity, Text } from 'react-native'
+export const RoundedButton = () => {
+ return (
+ <TouchableOpacity
+ onPress={() => {}}
+
+ >
+ <Text>ENTRAR</Text>
+ </TouchableOpacity>
+ )
+} 
